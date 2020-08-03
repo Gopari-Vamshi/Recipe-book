d7046ac80080dff197b0dca2ce9a0c38c04ddc1a
Gopari-Vamshi <vamshigopari6@gmail.com>
2020-08-03 23:03:35 +0530

    Recipe-Book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
   { path: 'shopping-list', loadChildren: () => import('./shopping-list/shoppinglist.module').then(m => m.ShoppinglistModule)},
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
